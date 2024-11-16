b390503e8704e8178620df131286e0dc117bfc17
NaTTaNMendes <nattan078@gmail.com>
2024-11-16 15:26:30 -0300

    Aplicando o back na selecao de perfil e historico

--- js/api.js
+++ js/api.js
@@ -65,6 +65,58 @@ async function login() {
     }
 }
 
+async function updateProfile() {
+
+    try {
+
+        let profileData = {
+            faceShape: localStorage.getItem("selectHeadType"),
+            hairCurl: localStorage.getItem("selectHairType")
+        };
+    
+        const response = await apiRequest("/api/users/profile", "PATCH", profileData, localStorage.getItem("token"));
+
+        if (response.status != 200) {
+            console.error("Erro ao enviar os dados do usuário.");
+        }
+    } catch (error) {
+        console.error("Erro na requisição de envio de dados do perfil:", error);
+    }
+}
+
+async function getPastAppointments() {
+    try {
+        const response = await apiRequest("/api/appointments/history", "GET", null, localStorage.getItem("token"));
+        const data = await response.json();
+        const container = document.getElementById("appointment-history");
+
+        for (let i = 0; i < data.length; i++) {
+            const row = document.createElement('tr');
+            const ap = data[i];
+
+            tempo_convertido = parseLocalDateTime(ap.startInterval);
+
+            row.innerHTML = `
+                <td>${tempo_convertido.dateValue}</td>
+                <td>${tempo_convertido.timeValue}</td>
+                <td>${ap.beautyItems.map(ap => ap.name).join(', ')}</td>
+                <td>${ap.employee.name}</td>
+            `;
+            container.appendChild(row);
+        }
+
+        if (container.children.length === 0) {
+            const emptyRow = document.createElement('tr');
+            emptyRow.classList.add('text-center');
+            emptyRow.innerHTML = '<td colspan="4">Nenhum histórico de agendamentos disponível.</td>';
+            container.appendChild(emptyRow);
+        }
+            
+    } catch (error) {
+        console.error('Erro ao buscar os dados:', error);
+    }
+}
+
 async function collectProfile() {
 
     try {
@@ -72,21 +124,48 @@ async function collectProfile() {
 
         if (response.status == 200) {
             const data = await response.json();
-            
+
+            const faceShapes = {
+                "ROUND": "Redondo",
+                "SQUARE": "Quadrado",
+                "TRIANGLE": "Triangular",
+                "OVAL": "Oval",
+                "DIAMOND": "Diamante"
+            };
+
+            const hairCurls = {
+                "STRAIGHT": "Liso",
+                "WAVY": "Ondulado",
+                "CURLY": "Cacheado",
+                "KINKY": "Crespo"
+            };
+
             if (data.faceShape != null) {
-                document.getElementById("faceShape").innerHTML = data.faceShape;
+                document.getElementById("faceShape").innerHTML = faceShapes[data.faceShape];
             } else {
                 document.getElementById("faceShape").innerHTML = "Nenhum";
             }
 
             if (data.hairCurl != null) {
-                document.getElementById("hairType").innerHTML = data.hairCurl;
+                document.getElementById("hairType").innerHTML = hairCurls[data.hairCurl];
             } else {
                 document.getElementById("hairType").innerHTML = "Nenhum";
             }
 
             if (data.name != null) {
                 document.getElementById("userName").innerHTML = data.name;
+            }
+
+            if (data.favoriteEmployeeName != null) {
+                document.getElementById("favoriteProfessional").innerHTML = data.favoriteEmployeeName;
+            } else {
+                document.getElementById("favoriteProfessional").innerHTML = "Nenhum";
+            }
+
+            if (data.loyaltyPoints != null) {
+                document.getElementById("points").innerHTML = data.loyaltyPoints;
+            } else {
+                document.getElementById("points").innerHTML = "Nenhum";
             }
 
         } else {
--- js/utils.js
+++ js/utils.js
@@ -41,6 +41,20 @@ async function apiRequest(endpoint, method = 'GET', body = null, token = null) {
     }
 }
 
+function parseLocalDateTime(localDateTime) {
+    const [date, time] = localDateTime.split('T');
+    const [year, month, day] = date.split('-');
+    const formattedDate = `${day}/${month}/${year}`;
+    const [hours, minutes, seconds] = time.split(':');
+    const hoursInUTC3 = parseInt(hours) - 3;
+    const formattedTime = `${hoursInUTC3 < 10 ? '0' + hoursInUTC3 : hoursInUTC3}:${minutes}`;
+
+    return {
+        dateValue: formattedDate,
+        timeValue: formattedTime
+    };
+}
+
 // Carregar o conteúdo da navbar
 fetch('navbar.html')
 .then(response => response.text())
